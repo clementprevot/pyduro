2861054fe5de17c0af569680410d66808bb820fa
Clément PRÉVOT <clement.prevot@gmail.com>
2022-11-15 15:28:04 +0100

    feat: Improve publishing script

--- publish.sh
+++ publish.sh
@@ -1,27 +1,39 @@
 #!/bin/env bash
 
-REPOSITORY="testpypi"
-
-while getopts hpr:t option; do
-    case "${option}" in
-        h)
-            echo """
+function usage() {
+    echo """
 ./publish.sh [-h] [-r <repository name>]|[-p]|[-t]
     -h                   - Display this help message
     -r <repository name> - Publish the package to the given repository (whose name should be available in .pypirc)
     -p                   - Publish the package to the public PyPi repository (you should have a 'pypi' repository in you .pypirc)
     -t                   - Default - Publish the package to the test PyPi repository (you should have a 'testpypi' repository in you .pypirc)
-            """
-            exit
+    """
+
+    exit 0
+}
+
+REPOSITORY="testpypi"
+
+while getopts hpr:t option; do
+    case "${option}" in
+        h)
+            usage
             ;;
+
         r)
             REPOSITORY=${OPTARG}
             ;;
+
         p)
             REPOSITORY="pypi"
             ;;
+
         t)
             REPOSITORY="testpypi"
+            ;;
+
+        *)
+            usage
             ;;
     esac
 done
